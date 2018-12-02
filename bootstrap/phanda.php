c83813282516a728ab77e6c91e7b97f006e57bc8
Storm Burpee <storm@phandaframework.com>
2018-12-02 19:47:29 +1030

    Work on kunfu commandline

---
+++ bootstrap/phanda.php
@@ -0,0 +1,17 @@
+<?php
+
+$phanda = new \Phanda\Foundation\Application(
+    $_ENV['PHANDA_BASE_PATH'] ?? dirname(__DIR__)
+);
+
+$phanda->singleton(
+    Phanda\Contracts\Http\Kernel::class,
+    \Phanda\Foundation\Http\Kernel::class
+);
+
+$phanda->singleton(
+    \Phanda\Contracts\Console\Kernel::class,
+    \Phanda\Foundation\Console\Kernel::class
+);
+
+return $phanda;

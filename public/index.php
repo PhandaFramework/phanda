7831575ba1a261bced77a4caefe5c8d254ce7aed
Storm Burpee <storm@phandaframework.com>
2018-12-04 01:21:56 +1030

    Bootstrapping the http kernel is now implemented

--- public/index.php
+++ public/index.php
@@ -1,1 +1,22 @@
 <?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+/**
+ * @var \Phanda\Contracts\Foundation\Application $phanda
+ */
+$phanda = require_once __DIR__.'/../bootstrap/phanda.php';
+
+/**
+ * @var \Phanda\Contracts\Http\Kernel $httpKernel
+ */
+$httpKernel = $phanda->create(\Phanda\Contracts\Http\Kernel::class);
+$request = \Phanda\Foundation\Http\Request::capture();
+
+$response = $httpKernel->handle(
+    $request
+);
+
+$response->send();
+
+$httpKernel->terminate($request, $response);

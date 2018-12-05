f28b9e3c30c21f3fb22ee3021717537aaf5e538c
Storm Burpee <storm@phandaframework.com>
2018-12-05 15:37:52 +1030

    Updating config

--- config/phanda.php
+++ config/phanda.php
@@ -2,13 +2,13 @@
 
 return [
 
-    'name' => environment('APPLICATION_NAME', 'Phanda'),
+    'name' => environment('app.APPLICATION_NAME', 'Phanda'),
 
-    'environment' => environment('APPLICATION_ENVIRONMENT', 'production'),
+    'environment' => environment('app.APPLICATION_ENVIRONMENT', 'production'),
 
-    'debug' => environment('APPLICATION_ALLOW_DEBUGGING', false),
+    'debug' => environment('app.APPLICATION_ALLOW_DEBUGGING', false),
 
-    'url' => environment('APPLICATION_URL', 'http://localhost'),
+    'url' => environment('app.APPLICATION_URL', 'http://localhost'),
 
     'providers' => [
         \Phanda\Providers\Foundation\BootstrapKungfuServiceProviders::class

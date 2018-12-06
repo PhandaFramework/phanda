ef3d74d42ddf7daaaea1a73c431d6d74ace386f2
Storm Burpee <storm@phandaframework.com>
2018-12-06 21:29:55 +1030

    using the Router facade

--- routing/base.php
+++ routing/base.php
@@ -1,9 +1,15 @@
 <?php
 
-router()->get('home', '/', function() {
+use Phanda\Support\Facades\Routing\Router;
+
+Router::get('home', '/', function() {
     return "Hello, World!";
 });
 
-router()->get('hello-person', '/hello/{name}', function($name) {
+Router::get('hello-person', '/hello/{name}', function($name) {
     return "Hello, {$name}!";
+});
+
+Router::get('test', '/test', function() {
+    return "test";
 });

499c7ecd481b979cfd0d6552aed4ad9c14f5211c
Storm Burpee <storm@phandaframework.com>
2018-12-09 13:56:36 +1030

    Finalised the welcome scene

--- assets/scenes/welcome.bamboo.php
+++ assets/scenes/welcome.bamboo.php
@@ -15,6 +15,12 @@
             <img src="{{ config('phanda.url') }}/images/phanda-logo.png">
             <h1>Welcome to the <span>phanda</span>framework</h1>
             <h2>Hello, {{ $name ?? "World" }}!</h2>
+            <div class="help-links">
+                <a href="https://phandaframework.com" target="_blank">Phanda Website</a>
+                <a href="https://phandaframework.com/docs" target="_blank">Documentation</a>
+                <a href="https://phandaframework.com/news" target="_blank">News</a>
+                <a href="https://gitlab.com/phanda/framework" target="_blank">Source Code</a>
+            </div>
         </div>
     </body>
 </html>

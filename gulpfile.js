6cff7b2e5ef775bda7812a730fbc038dc565b978
Storm Burpee <storm@phandaframework.com>
2018-12-09 13:26:51 +1030

    Added in gulp

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    sourcemaps = require("gulp-sourcemaps"),
+    uglify = require('gulp-uglify');
+
+const paths = {
+    styles: {
+        src: "assets/sass/**/*.scss",
+        destination: "public/css"
+    },
+    js: {
+        src: "assets/js/**/*.js",
+        destination: "public/js"
+    }
+};
+
+function styles() {
+    return (
+        gulp
+            .src(paths.styles.src)
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .on("error", sass.logError)
+            .pipe(postcss([autoprefixer(), cssnano()]))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(paths.styles.destination))
+    );
+}
+
+function javascript() {
+    return (
+        gulp
+            .src(paths.js.src)
+            .pipe(sourcemaps.init())
+            .pipe(uglify())
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(paths.js.destination))
+    );
+}
+
+function watch() {
+    styles();
+    javascript();
+
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.js.src, javascript());
+}
+
+function build()
+{
+    styles();
+    javascript();
+}
+
+exports.styles = styles;
+exports.javascript = javascript;
+exports.watch = watch;
+exports.build = build;

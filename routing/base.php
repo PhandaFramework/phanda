3d11f6520b9ec7d91c6182b2368db6dbde1f9fed
Storm Burpee <storm@phandaframework.com>
2018-12-09 20:22:43 +1030

    Updated the routing example file, to explain the naming of routes

--- routing/base.php
+++ routing/base.php
@@ -3,11 +3,14 @@
 use Phanda\Support\Facades\Routing\Router;
 use Phanda\Support\Facades\Scene\Scene;
 
-Router::get('/', function() {
+Router::get('/', function () {
     return Scene::create('welcome');
 }, 'home');
 
-Router::prefix('/hello')->group(function() {
-   Router::get('/', 'HelloWorldController@index', 'hello-world');
-   Router::get('/{name}', 'HelloWorldController@name', 'hello-name');
+Router::prefix('/hello')->name('hello')->group(function () {
+    // By naming a group, it automatically prepends the name to subsequent routes.
+    // I.e., this group is named 'hello' and the next route is named 'world'. This automatically
+    // Get's prepended to be 'hello.world'
+    Router::get('/', 'HelloWorldController@index', 'world');
+    Router::get('/{name}', 'HelloWorldController@name', 'name');
 });

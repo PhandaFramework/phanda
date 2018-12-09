04754d9152af18b3567f05f1d175e70dfaab5eba
Storm Burpee <storm@phandaframework.com>
2018-12-09 20:13:55 +1030

    Updating the routes file to use the new way of defining names

--- routing/base.php
+++ routing/base.php
@@ -3,11 +3,11 @@
 use Phanda\Support\Facades\Routing\Router;
 use Phanda\Support\Facades\Scene\Scene;
 
-Router::get('home', '/', function() {
+Router::get('/', function() {
     return Scene::create('welcome');
-});
+}, 'home');
 
 Router::prefix('/hello')->group(function() {
-   Router::get('hello-world', '/', 'HelloWorldController@index');
-   Router::get('hello-name', '/{name}', 'HelloWorldController@name');
+   Router::get('/', 'HelloWorldController@index', 'hello-world');
+   Router::get('/{name}', 'HelloWorldController@name', 'hello-name');
 });

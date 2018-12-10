78679fc48f6787b538f2b0f124d72be059c26c2b
Storm Burpee <storm@phandaframework.com>
2018-12-11 03:50:02 +1030

    Updated The helloworld controller to show examples of returning responses using the responsemanager.

--- app/Controllers/HelloWorldController.php
+++ app/Controllers/HelloWorldController.php
@@ -8,13 +8,19 @@ class HelloWorldController extends \Phanda\Routing\Controller\AbstractController
 {
     public function index()
     {
-        return Scene::render('welcome');
+        // This is an example of using the ResponseManager to create a response. The response manager provides you
+        // With an easy way to generate different responses needed in your application. Please look at the docs for
+        // any further information.
+        return responseManager()->createSceneResponse('welcome');
     }
 
     public function name($name)
     {
         $name = ucfirst(strtolower($name));
 
+        // Here we are using the Scene facade to return a response to show you one of the many ways Phanda can
+        // Handle the generation and rendering of responses. Please consult the documentation for further
+        // Information on Phanda and Responses.
         return Scene::render('welcome', [
             "name" => $name
         ]);

9448d3a527a136d4a213df27fce7e1651e36ad43
Storm Burpee <storm@phandaframework.com>
2018-12-11 16:51:44 +1030

    Adding configuration for error scenes

--- config/scene.php
+++ config/scene.php
@@ -5,5 +5,7 @@ return [
         assets_path('scenes')
     ],
 
+    'error_scenes_path' => assets_path('scenes/errors'),
+
     'cachedPath' => environment('scene.CACHED_SCENE_PATH', realpath(storage_path('phanda/scenes')))
 ];

9bde7f8b77d06aede99a1e38ead8eae59b377f8d
Storm Burpee <storm@phandaframework.com>
2018-12-15 18:33:19 +1030

    Updating configuration options

--- config/database.php
+++ config/database.php
@@ -2,12 +2,15 @@
 
 return [
     'connections' => [
+        // For documentation in regards to the configuration of database drivers
+        // Please consult the Phanda docs for all of the configuration options.
         'default' => [
             'driver' => environment('database.DATABASE_DRIVER', 'mysql'),
             'host' => environment('database.DATABASE_HOST', '127.0.0.1'),
-            'database' => environment('database.DATABASE_NAME', 'localdatabase'),
-            'username' => environment('database.DATABASE_USERNAME', 'localuser'),
-            'password' => environment('database.DATABASE_PASSWORD', 'localpassword')
+            'port' => environment('database.DATABASE_PORT', '3306'),
+            'database' => environment('database.DATABASE_NAME', 'phanda'),
+            'username' => environment('database.DATABASE_USERNAME', 'root'),
+            'password' => environment('database.DATABASE_PASSWORD', '')
         ]
     ],
 

b19e23ffc52e8eb34821fbf0ad1453924b591430
Storm Burpee <storm@phandaframework.com>
2018-12-19 23:14:27 +1030

    Added in the commands configuration file, and added in the example HelloWorld file

---
+++ app/Console/Commands/HelloWorldCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Phanda\Console\ConsoleCommand;
+
+class HelloWorldCommand extends ConsoleCommand
+{
+	/**
+	 * @var string
+	 */
+	protected $signature = "hello:world {name?}";
+
+	/**
+	 * @var string
+	 */
+	protected $description = "This is an example command that is provided in your /app/Console/Commands folder.";
+
+	/**
+	 * Handles the command when it is executed.
+	 *
+	 * Please refer to the Phanda docs for more information.
+	 */
+	public function handle()
+	{
+		$name = $this->getInputArgument('environment') ?? "World";
+		$this->info("Hello, {$name}!");
+	}
+
+}
---
+++ config/commands.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	'folders' => [
+		// You can register folders in your application here, where you want to load any command from.
+		app_path('Console/Commands')
+	],
+
+	'commands' => [
+		// You can register commands here, if you choose not to register a commands folder.
+	],
+];
